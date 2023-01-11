da06c7b1f1497103c6185565da9af820d32df116
monarie <jobmonari7@gmail.com>
2023-01-11 03:30:24 +0300

    final api

--- Go REST APIs/GIN 002/config/db.go
+++ Go REST APIs/GIN 002/config/db.go
@@ -10,7 +10,7 @@ import (
 var DB *gorm.DB
 
 func Connect() {
-	db, err := gorm.Open(postgres.Open("postgres://postgres@localhost:5432/golangcrud?sslmode=disable"), &gorm.Config{})
+	db, err := gorm.Open(postgres.Open("postgres://postgres:docker@localhost:5400/golangcrud?sslmode=disable"), &gorm.Config{})
 	if err != nil {
 		panic(err)
 	}
--- Go REST APIs/GIN 002/controller/user.go
+++ Go REST APIs/GIN 002/controller/user.go
@@ -7,8 +7,35 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
-func UserController(c *gin.Context) {
+func GetUsers(c *gin.Context) {
 	users := []models.User{}
 	config.DB.Find(&users)
 	c.JSON(200, &users)
+}
+
+func DeleteUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).Delete(&user)
+	c.JSON(200, &user)
+}
+
+func CreateUser(c *gin.Context) {
+	var user models.User
+	c.BindJSON(&user)
+	config.DB.Create(&user)
+	c.JSON(200, &user)
+}
+
+func UpdateUser(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).First(&user)
+	c.BindJSON(&user)
+	config.DB.Save(&user)
+	c.JSON(200, &user)
+}
+
+func GetUserById(c *gin.Context) {
+	var user models.User
+	config.DB.Where("id = ?", c.Param("id")).Find(&user)
+	c.JSON(200, &user)
 }
