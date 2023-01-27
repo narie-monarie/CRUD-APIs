6494d3a09a3623fe1c24f0493665bb18ff6882a9
monarie <jobmonari7@gmail.com>
2023-01-27 10:59:03 +0300

    adding req and res

--- Express_Rest/Express01/index.js
+++ Express_Rest/Express01/index.js
@@ -1,13 +1,21 @@
 const http = require('http')
-const port = 3000
+
 const server = http.createServer((req, res) => {
-  if (req.method === 'GET', res === '/') {
+
+  if (req.method === 'GET' && res.url === '/') {
     console.log('Hello World')
+    res.end()
+  }
+
+  if (req.method === 'GET' && res.url === '/hello') {
+    res.writeHead(200, { 'Content-Type': 'text/html' })
+    res.write('<h1>Hello World</h1>')
+    console.log(req.url)
     res.end()
   }
 
 })
 
-server.listen(port, () => {
+server.listen(3000, () => {
   console.log(`server listening on port 3000`)
 })
