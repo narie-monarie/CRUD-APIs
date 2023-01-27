a1af5d20fe63c51b165895aab9c48a4e42fde0cf
monarie <jobmonari7@gmail.com>
2023-01-27 11:00:52 +0300

    req !=

--- Express_Rest/Express01/index.js
+++ Express_Rest/Express01/index.js
@@ -2,12 +2,12 @@ const http = require('http')
 
 const server = http.createServer((req, res) => {
 
-  if (req.method === 'GET' && res.url === '/') {
+  if (req.method === 'GET' && req.url === '/') {
     console.log('Hello World')
     res.end()
   }
 
-  if (req.method === 'GET' && res.url === '/hello') {
+  if (req.method === 'GET' && req.url === '/hello') {
     res.writeHead(200, { 'Content-Type': 'text/html' })
     res.write('<h1>Hello World</h1>')
     console.log(req.url)
